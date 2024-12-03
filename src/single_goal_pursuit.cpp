e4bd8a4eef9b3b85f8b497699a39842a89bd58e9
umiklos <ungermiklos1992@gmail.com>
2024-12-03 15:11:34 +0100

    add crio publisher

--- src/single_goal_pursuit.cpp
+++ src/single_goal_pursuit.cpp
@@ -15,9 +15,11 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/pose_array.hpp"
 #include "rcl_interfaces/msg/set_parameters_result.hpp"
+#include "crio_ros2_msgs/msg/crio_message.hpp"
 
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2/LinearMath/Matrix3x3.h"
+
 //#include "wayp_plan_tools/common.hpp"
 
 std::string waypoint_topic = "pursuitgoal";
